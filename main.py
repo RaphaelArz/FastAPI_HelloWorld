d6948c7b8b08b706b468ff95bcc2091e7f51ac7c
Raphael <raphaelarzoin173@gmail.com>
2025-07-10 12:45:57 +0200

    ,

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Hell0oo, World!"}
+    return {"message": "Hello, World!"}
 
 if __name__ == "__main__":
     import uvicorn
